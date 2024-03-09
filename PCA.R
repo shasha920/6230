727e38d6a964c12a3e8e18cb6d32b96154c3ee97
shasha920 <96502556+shasha920@users.noreply.github.com>
2024-03-09 09:21:27 -0800

    Add files via upload

---
+++ PCA.R
@@ -0,0 +1,61 @@
+# Principle Component Analysis
+
+install.packages("corrr")
+install.packages("ggcorrplot")
+install.packages("FactoMineR")
+install.packages("factoextra")
+install.packages("pls")
+
+# Simulation
+set.seed(101)
+N = 1000
+x = rchisq(N,2)
+y = rnorm(N,0,2) + x
+z = rnorm(N,0,1) + x
+data = data.frame(x,y,z)
+corr_matrix <- cor(data)
+
+data.pca <- princomp(corr_matrix)
+summary(data.pca)
+data.pca$loadings[, 1:3]
+fviz_pca_var(data.pca, col.var = "black")
+
+# Real data set (mtcars)
+
+data = mtcars
+
+a <- pcr(hp~mpg+disp+drat+wt+qsec, data=mtcars, scale=TRUE, validation="CV")
+
+summary(a)
+
+a$coefficients
+
+a$scores
+
+summary(lm(lprice~a$scores))
+
+forecasting = a$fitted.values
+
+validationplot(a)
+validationplot(a, val.type="MSEP")
+validationplot(a, val.type="R2")
+
+# Real data set (hprice2)
+attach(hprice2)
+
+a<- pcr(lprice~crime+nox+rooms+dist+radial+proptax+stratio+lowstat,scale=TRUE,validation="CV")
+
+summary(a)
+
+a$coefficients
+
+a$scores
+
+summary(lm(lprice~a$scores))
+
+forecasting = a$fitted.values
+
+validationplot(a)
+validationplot(a, val.type="MSEP")
+validationplot(a, val.type="R2")
+
